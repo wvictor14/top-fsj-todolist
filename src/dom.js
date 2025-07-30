593dad0ba5083c707dd60a88a6f75197df00e414
Victor Yuan <victor.2wy@gmail.com>
2025-07-30 15:05:30 -0700

    start dom

---
+++ src/dom.js
@@ -0,0 +1,11 @@
+// this module is for populating the dom
+
+export function addProject(project) {
+  // parent div
+  const parent = document.getElementsByClassName("projects");
+
+  // new div for a project
+  const child = document.createElement("div");
+  newDiv.textContent = project.title;
+
+}
