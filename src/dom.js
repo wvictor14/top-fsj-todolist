1ceef29c5182057e82855cef46546190e94baa68
Victor Yuan <victor.2wy@gmail.com>
2025-07-30 15:08:06 -0700

    addproject to dom

--- src/dom.js
+++ src/dom.js
@@ -2,10 +2,12 @@
 
 export function addProject(project) {
   // parent div
-  const parent = document.getElementsByClassName("projects");
+  const parent = document.querySelector(".projects")
 
   // new div for a project
   const child = document.createElement("div");
-  newDiv.textContent = project.title;
+  child.textContent = project.title;
+
+  parent.appendChild(child);
 
 }
