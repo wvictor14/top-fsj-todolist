8d6af4557ffdecc2a495cf19dc9e79c32c3c28f6
Victor Yuan <victor.2wy@gmail.com>
2025-07-30 15:29:37 -0700

    addToDo

--- src/dom.js
+++ src/dom.js
@@ -2,7 +2,7 @@
 
 export function addProject(project) {
   // parent div
-  const parent = document.querySelector(".projects")
+  const parent = document.querySelector(".projects");
 
   // new div for a project
   const child = document.createElement("div");
@@ -10,4 +10,48 @@ export function addProject(project) {
 
   parent.appendChild(child);
 
+}
+
+export function addTodo(todo) {
+// parent div
+  const parent = document.querySelector(".lists");
+
+  // new child div
+  const child = document.createElement('div');
+
+  // content of child elements
+  const title = document.createElement("span");
+  title.textContent = todo.title;
+  title.classList.add('title');
+
+  const desc = document.createElement("span");
+  desc.textContent = ' ' + todo.description;
+  desc.classList.add('desc');
+
+  const duedate = document.createElement('span');
+  duedate.textContent = ' ' + todo.dueDate;
+  duedate.classList.add('duedate');
+
+  const priority = document.createElement('span');
+  priority.textContent = ' ' + todo.priority;
+  priority.classList.add('priority');
+
+  const notes = document.createElement('span');
+  notes.textContent = ' ' + todo.notes;
+  notes.classList.add('notes');
+
+  // loop over and append them
+  const childElements = [
+    title,
+    desc,
+    duedate,
+    priority,
+    notes
+  ];
+
+  for (let i = 0; i < childElements.length; i ++) {
+    child.appendChild(childElements[i]);
+  }
+
+  parent.appendChild(child);
 }
